9e069a96db8f7b19db0227cfd43e42597355611f
Victor Zamora <victorzamoragutierrez@gmail.com>
2016-09-11 14:13:36 -0500

    Primer ejercicio. Saludirijillos

---
+++ Practica1/1_6.py
@@ -0,0 +1,36 @@
+# Problema 1.6 de "Numerical Mathematics and Computing"
+
+from numpy import linspace
+
+# Primer metodo de suma (xk = xk-1 + h)
+# a, b - Extremos del intervalo a sumar
+# n - numero de elementos menos 1
+def suma1(a, b, n):
+    lista_x = [] # Lista de xi's
+    lista_x.append(a)
+    h = float(a + b)/n # Tamano del paso
+    for num in linspace(a, b, n):
+        lista_x.append(lista_x[-1] + h)
+    return lista_x
+
+# Segundo metodo de suma (xk = a + kh)
+# a, b - Extremos del intervalo a sumar
+# n - numero de elementos menos 1
+def suma2(a, b, n):
+    lista_x = [] # Lista de xi's
+    h = float(a + b)/n # Tamano del paso
+    k = 0 # Contador
+    for num in linspace(a, b, n+1):
+        lista_x.append(a + k*h)
+        k += 1
+    return lista_x
+
+print('Ilustremos la diferencia entre ambos métodos con 10 números en el intervalo [0, 1]')
+lista1 = suma1(0, 1, 10)
+lista2 = suma2(0, 1, 10)
+contador = 0
+for num in lista1:
+    print('Primer método: ' + 'x' + str(contador) + '= ' + str(num))
+    print('Segundo método: ' + 'x' + str(contador) + '= ' + str(lista2[contador]))
+    print('Diferencia:' + str(num - lista2[contador]))
+    contador += 1
